07dd6c9927d5711c5bc54d6f89e436fdf6aeea9b
Ajit Jadhav <ajitjadhav2310@outlook.com>
2020-05-02 23:01:11 +0530

    file updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps Capstone
+          DevOps Capstone
         </a>
       </header>
     </div>
